be283903727adb4126540f4149daddefa51fbd7c
mehmet-mhy <mehmet.mhy@gmail.com>
2026-07-19 04:17:09 -0600

    backup

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ PROCESS_SCRIPT = os.path.join(ROOT_DIR, "process.py")
 GET_SCRIPT = os.path.join(ROOT_DIR, "retrieve.py")
 
 PY_CALL = os.path.join(ROOT_DIR, "env/bin/python3")
-if os.path.isfile(PY_CALL) == False:
+if not os.path.isfile(PY_CALL):
     PY_CALL = "python3"
 
 targets = [BUILD_SCRIPT, PROCESS_SCRIPT, GET_SCRIPT]
