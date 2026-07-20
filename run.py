8d616020bfaafe0a05d2beac4ef5f346b1d47907
mehmet-mhy <mehmet.mhy@gmail.com>
2026-07-20 02:05:00 -0600

    backup

--- run.py
+++ run.py
@@ -11,7 +11,15 @@ PY_CALL = os.path.join(ROOT_DIR, "env/bin/python3")
 if not os.path.isfile(PY_CALL):
     PY_CALL = "python3"
 
-targets = [BUILD_SCRIPT, PROCESS_SCRIPT, GET_SCRIPT]
+just_update = False
+args = list(sys.argv)[1:]
+if len(args) > 0 and args[0].lower() in ["-u", "--update"]:
+    just_update = True
+
+targets = [BUILD_SCRIPT, PROCESS_SCRIPT]
+if not just_update:
+    targets.append(GET_SCRIPT)
+
 for i in range(len(targets)):
     cmd = f"{PY_CALL} {targets[i]}"
     status = os.system(cmd)
